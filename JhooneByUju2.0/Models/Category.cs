eff9aea9b7c482b478bac7030f21a02a17a16b81
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-04-20 13:16:11 -0500

    add categories model with data annotations

---
+++ JhooneByUju2.0/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JhooneByUju2._0.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Display Order")]
+        public string DisplayOrder { get; set; }
+
+    }
+}
