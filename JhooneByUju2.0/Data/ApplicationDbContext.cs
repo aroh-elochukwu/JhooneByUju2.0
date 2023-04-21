6bfbc261904360fb467729fd0e767b67db3f92fd
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-04-20 20:56:04 -0500

    setup applicationDbContext and create category table on db

---
+++ JhooneByUju2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JhooneByUju2._0.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JhooneByUju2._0.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {                
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
