1ee44fc644b2385e568a2fbc7e4e8df6994d8db6
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-04-25 08:25:27 -0500

    add bootswatch theme

--- JhooneByUju2.0/Controllers/CategoryController.cs
+++ JhooneByUju2.0/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JhooneByUju2._0.Data;
+using JhooneByUju2._0.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JhooneByUju2._0.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List <Category> categoriesList = _db.Categories.ToList();
+
+            return View(categoriesList);
         }
     }
 }
