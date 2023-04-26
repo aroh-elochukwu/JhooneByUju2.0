e373bd8900d6ff2744caad0e672068a23d97fd90
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-04-25 19:52:57 -0500

    add edit category functionality

--- JhooneByUju2.0/Controllers/CategoryController.cs
+++ JhooneByUju2.0/Controllers/CategoryController.cs
@@ -17,7 +17,8 @@ namespace JhooneByUju2._0.Controllers
 
             return View(categoriesList);
         }
-        
+
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -41,6 +42,39 @@ namespace JhooneByUju2._0.Controllers
             return View();
 
             
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            Category queriedCategory = _db.Categories.Find(id);
+
+            if (queriedCategory == null)
+            {
+                return NotFound();
+
+            }
+            return View(queriedCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {            
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+
         }
     }
 }
