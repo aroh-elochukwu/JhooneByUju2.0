9fbddb27d91a9b6bbd28143e7088268f7d2638c9
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-05-09 09:21:24 -0500

    add imageUrl to product model and migrate to db

--- JhooneByUju.Models/Product.cs
+++ JhooneByUju.Models/Product.cs
@@ -35,5 +35,6 @@ namespace JhooneByUju.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
