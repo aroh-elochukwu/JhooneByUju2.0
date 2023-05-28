b898f0009d7f4f244a137e900498d3c6dc50392e
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-05-28 11:18:50 -0500

    fix bug introduced by scafolding identity

--- JhooneByUju.DataAccess/Data/ApplicationDbContext.cs
+++ JhooneByUju.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using JhooneByUju.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace JhooneByUju.DataAccess.Data
 {
-    public class ApplicationDbContext: IdentityDbContext
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {                
