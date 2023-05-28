e37be9f9845f77b457fc5aa312eac170733f1d4b
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-05-28 11:11:08 -0500

    scaffold identity

--- JhooneByUju.DataAccess/Data/ApplicationDbContext.cs
+++ JhooneByUju.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using JhooneByUju.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace JhooneByUju.DataAccess.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {                
@@ -14,6 +15,8 @@ namespace JhooneByUju.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Hand Bags", DisplayOrder = 0 },
                 new Category { Id = 2, Name = "Purses", DisplayOrder = 0 },
