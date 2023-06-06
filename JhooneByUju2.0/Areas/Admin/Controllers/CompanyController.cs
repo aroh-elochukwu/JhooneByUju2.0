b262b1af0472c07bdd842b75c7e7a183a8e45561
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-06-06 14:38:10 -0500

    merge companyController actions to give upsert

--- JhooneByUju2.0/Areas/Admin/Controllers/CompanyController.cs
+++ JhooneByUju2.0/Areas/Admin/Controllers/CompanyController.cs
@@ -29,87 +29,76 @@ namespace JhooneByUju2._0.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                //create
+                return View(new Company());
+            }
+            else
+            {
+                //update
+                Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(companyObj);
+            }
+
+        }
         [HttpPost]
-        public IActionResult Create(Company company)
+        public IActionResult Upsert(Company CompanyObj)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Company.Add(company);
+
+                if (CompanyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(CompanyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(CompanyObj);
+                }
+
                 _unitOfWork.Save();
-                TempData["success"] = "Company created";
-                return RedirectToAction("Index");
-
-            }
-            
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Edit(int? id)
-        {
-            if (id == 0 || id == null  )
-            {
-                return NotFound();                
-            }
-
-            Company? queriedCompany = _unitOfWork.Company.Get(u  => u.Id == id);
-
-            if (queriedCompany == null)
-            {
-                return NotFound();
-            }
-
-            return View(queriedCompany);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Company company)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Company.Update(company); 
-                _unitOfWork.Save();
-                TempData["success"] = "Company info Updated";
+                TempData["success"] = "Company created successfully";
                 return RedirectToAction("Index");
             }
-           
+            else
+            {
 
-            return View();
+                return View(CompanyObj);
+            }
         }
 
+
+        #region API CALLS
+
         [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objCompanyList });
+        }
+
+
+        [HttpDelete]
         public IActionResult Delete(int? id)
         {
-            if (id == 0 || id == null)
+            var CompanyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+            if (CompanyToBeDeleted == null)
             {
-                return NotFound();
+                return Json(new { success = false, message = "Error while deleting" });
             }
 
-            Company? queriedCompany = _unitOfWork.Company.Get(u => u.Id == id);
-
-            if (queriedCompany == null)
-            {
-                return NotFound();
-
-            }
-            return View(queriedCompany);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePost(int? id)
-        {
-            Company? company = _unitOfWork.Company.Get(u => u.Id == id);
-
-            if (company == null)
-            {
-                return NotFound();
-            }
-
-            _unitOfWork.Company.Remove(company);
+            _unitOfWork.Company.Remove(CompanyToBeDeleted);
             _unitOfWork.Save();
-            TempData["success"] = "Company deleted";
-            return RedirectToAction("Index");
 
+            return Json(new { success = true, message = "Delete Successful" });
         }
+
+        #endregion
+
+
     }
 }
