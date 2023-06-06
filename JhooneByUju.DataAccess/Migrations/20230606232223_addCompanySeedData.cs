7c6b824f4a5b53878fbcb2ae6da69ca0be8b5db6
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-06-06 18:24:25 -0500

    add company seed data to database

---
+++ JhooneByUju.DataAccess/Migrations/20230606232223_addCompanySeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace JhooneByUju.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCompanySeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Warri", "JG ALSP", "08022347998", "WE 332", "Delta State", "9 Igbudu Market" },
+                    { 2, "Winnipeg", "Market Square", "4317839374", "YSD 922", "Manitoba", "300 Main Street" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
