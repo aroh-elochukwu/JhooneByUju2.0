e7d4d29bd279c03f2b709016b7af5fa7b2d99f9c
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-06-06 10:26:13 -0500

    create company model and migrate to db

--- JhooneByUju.DataAccess/Data/ApplicationDbContext.cs
+++ JhooneByUju.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace JhooneByUju.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet <Product> Products { get; set; }   
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
