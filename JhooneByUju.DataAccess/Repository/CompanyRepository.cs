f5cfe78ef6f8e2ed460a5c9099a0bc6993219376
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-06-06 10:55:33 -0500

    add company to repository and unitOfWork

---
+++ JhooneByUju.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,37 @@
+﻿using JhooneByUju.DataAccess.Data;
+using JhooneByUju.DataAccess.Repository.IRepository;
+using JhooneByUju.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JhooneByUju.DataAccess.Repository
+{
+    public class CompanyRepository: Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base (db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company company)
+        {
+            Company queriedCompany = _db.Companies.FirstOrDefault(x => x.Id == company.Id);
+
+            if (queriedCompany != null)
+            {
+                queriedCompany.Name = company.Name;
+                queriedCompany.StreetAddress = company.StreetAddress;
+                queriedCompany.City = company.City;
+                queriedCompany.State = company.State;
+                queriedCompany.PostalCode = company.PostalCode;
+                queriedCompany.PhoneNumber = company.PhoneNumber;
+
+            }
+        }
+    }
+}
--- JhooneByUju.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ JhooneByUju.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,6 +10,8 @@ namespace JhooneByUju.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
+        
         void Save();
 
     }
