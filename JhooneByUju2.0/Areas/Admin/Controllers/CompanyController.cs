4978a8ab6347a5b4706ba4121ff165c802308999
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-06-06 12:24:37 -0500

    create company controller with basic crud operations

---
+++ JhooneByUju2.0/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,45 @@
+﻿using JhooneByUju.DataAccess.Repository.IRepository;
+using JhooneByUju.Models;
+using JhooneByUju.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JhooneByUju2._0.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            
+        }
+        public IActionResult Index()
+        {
+            List<Company> companies = _unitOfWork.Company.GetAll().ToList();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Company.Add(company);
+                _unitOfWork.Save();
+                TempData["success"] = "Company created";
+                return RedirectToAction("Index");
+
+            }
+            
+            return View();
+        }
+    }
+}
