d20631cc5b967d48aa45cb23d8b461384034252e
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-06-12 09:07:19 -0600

    Add shopping cart to repository and unit of work

---
+++ JhooneByUju.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,15 @@
+﻿using JhooneByUju.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JhooneByUju.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository: IRepository<ShoppingCart>
+    {
+        void Update(ShoppingCart shoppingCart);
+        
+    }
+}
