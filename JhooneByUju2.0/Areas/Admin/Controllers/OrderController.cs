423c7edeee0bfdfe572509d79c0a19df0394ec53
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-06-23 09:06:39 -0500

    add OrderController and OrderVM

---
+++ JhooneByUju2.0/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using JhooneByUju.DataAccess.Repository.IRepository;
+using JhooneByUju.Models;
+using JhooneByUju.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JhooneByUju2._0.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork) 
+        { 
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            return Json(new { data = orderHeaders });
+        }
+
+
+        #endregion
+    }
+
+
+}
