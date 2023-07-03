ccafd19c7f877b9cf26a007fb544958c01332c5f
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-07-03 15:33:20 -0500

    update cart quantity upon item remove

--- JhooneByUju2.0/Areas/Customer/Controllers/CartController.cs
+++ JhooneByUju2.0/Areas/Customer/Controllers/CartController.cs
@@ -201,11 +201,12 @@ namespace JhooneByUju2._0.Areas.Customer.Controllers
 
         public IActionResult Minus(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked : true);
             if (cartFromDb.Count <= 1)
             {
                 //remove item from cart
-                _unitOfWork.ShoppingCart.Remove(cartFromDb);                
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);                   
             }
             else {
                 // reduce number of item in cart
@@ -219,7 +220,8 @@ namespace JhooneByUju2._0.Areas.Customer.Controllers
 
         public IActionResult Remove(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked : true);
+            HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() -1);
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
             _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
